c6a55cbb7a9ea1fd1cc50aef129e59c4ad860b5d
eteries <internet.practica@gmail.com>
2017-04-23 21:32:46 +0300

    module2-task1: Categories dropdown & lots list : Fix : Implement requested changes

--- index.php
+++ index.php
@@ -36,42 +36,37 @@ $lots = [
          'category' => 'Доски и лыжи',
          'price' => 10999,
          'img' => 'img/lot-1.jpg',
-
-    ],
-    [
+     ],
+     [
         'title' => 'DC Ply Mens 2016/2017 Snowboard',
         'category' => 'Доски и лыжи',
         'price' => 159999,
         'img' => 'img/lot-2.jpg',
-
-    ],
-    [
+     ],
+     [
         'title' => 'Крепления Union Contact Pro 2015 года размер L/XL',
         'category' => 'Крепления',
         'price' => 8000,
         'img' => 'img/lot-3.jpg',
-
-    ],
-    [
+     ],
+     [
         'title' => 'Ботинки для сноуборда DC Mutiny Charocal',
         'category' => 'Ботинки',
         'price' => 10999,
         'img' => 'img/lot-4.jpg',
-
-    ],
-    [
+     ],
+     [
         'title' => 'Куртка для сноуборда DC Mutiny Charocal',
         'category' => 'Одежда',
         'price' => 7500,
         'img' => 'img/lot-5.jpg',
-
-    ],
-    [
+     ],
+     [
         'title' => 'Маска Oakley Canopy',
         'category' => 'Разное',
         'price' => 5400,
         'img' => 'img/lot-6.jpg',
-    ]
+     ]
 ];
 
 ?>
@@ -139,27 +134,27 @@ $lots = [
             <h2>Открытые лоты</h2>
             <select class="lots__select">
                 <option>Все категории</option>
-                <?php foreach($categories as $category_name): ?>
-                <option><?=$category_name;?></option>
+                <?php foreach ($categories as $category_name): ?>
+                <option><?= $category_name ?></option>
                 <?php endforeach; ?>
             </select>
         </div>
         <ul class="lots__list">
-            <?php foreach($lots as $lot_key => $lot_value): ?>
+            <?php foreach ($lots as $lot): ?>
             <li class="lots__item lot">
                 <div class="lot__image">
-                    <img src=<?=$lot_value['img'];?> width="350" height="260" alt="Сноуборд">
+                    <img src=<?= $lot['img'] ?> width="350" height="260" alt="<?= $lot['title'] ?>">
                 </div>
                 <div class="lot__info">
-                    <span class="lot__category"><?=$lot_value['category'];?></span>
-                    <h3 class="lot__title"><a class="text-link" href=""><?=$lot_value['title'];?></a></h3>
+                    <span class="lot__category"><?= $lot['category'] ?></span>
+                    <h3 class="lot__title"><a class="text-link" href=""><?= $lot['title'] ?></a></h3>
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost"><?=$lot_value['price'];?><b class="rub">р</b></span>
+                            <span class="lot__cost"><?= $lot['price'] ?><b class="rub">р</b></span>
                         </div>
                         <div class="lot__timer timer">
-                            <?=$lot_time_remaining;?>
+                            <?= $lot_time_remaining ?>
                         </div>
                     </div>
                 </div>
