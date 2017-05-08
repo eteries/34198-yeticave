0e33f0fccc0b1d5788ca440a44a6ea8834a1c695
eteries <internet.practica@gmail.com>
2017-05-08 14:45:12 +0300

    module4-task1: Fix : Implement requested changes (2)

--- login.php
+++ login.php
@@ -4,52 +4,47 @@ session_start();
 require_once 'functions.php';
 require_once 'userdata.php';
 
-$login = [];
-$invalid_controls = [];
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $login = [];
+    $invalid_controls = [];
 
-/**
- * Сформировать массив не валидных полей, если таковые найдутся.
- */
-
-if (isset($_POST['email']) && !filter_var(trim($_POST['email']), FILTER_VALIDATE_EMAIL)) {
-    $invalid_controls['email'] = 'Введите e-mail';
-}
-
-if (isset($_POST['password']) && empty(trim($_POST['password']))) {
-    $invalid_controls['password'] = 'Введите пароль';
-}
-
-/**
- * Если данные введены без ошибок, сформировать пару e-mail/пароль для процедуры логина
- */
-if (empty($invalid_controls) && !empty($_POST)) {
-    $login['email'] = trim($_POST['email'] ?? '');
-    $login['password'] = trim($_POST['password'] ?? '');
-}
-
-/**
- * Если данные для логина сформированы, идёт проверка по e-mail
- */
-if (!empty($login)) {
-    $check = array_search($login['email'], array_column($users, 'email'), true);
-}
-
-// Если в ходе проверки, пользователь с этим e-mail находится, то для авторизации сверяется пароль
-if (isset($check) && $check !== false) {
-    $this_user = $users[$check];
-    $verify = password_verify($login['password'], $this_user['password']);
-
-    if ($verify === true) {
-        $_SESSION['user'] = $this_user;
-        header('location: /');
-        exit();
-    } else {
-        $invalid_controls['password'] = 'Вы ввели неверный пароль';
+    /**
+     * Сформировать массив не валидных полей, если таковые найдутся.
+     */
+    if (!filter_var(trim($_POST['email']), FILTER_VALIDATE_EMAIL)) {
+        $invalid_controls['email'] = 'Введите e-mail';
     }
-}
-// Если в ходе проверки, пользователь с этим e-mail не находится, выводится соответствующее сообщение
-if (isset($check) && $check === false) {
-    $invalid_controls['email'] = 'Пользователь не найден';
+
+    if (empty(trim($_POST['password']))) {
+        $invalid_controls['password'] = 'Введите пароль';
+    }
+
+    /**
+     * Если данные введены без ошибок, сформировать пару e-mail/пароль для процедуры логина
+     */
+    if (empty($invalid_controls)) {
+        $login['email'] = trim($_POST['email'] ?? '');
+        $login['password'] = trim($_POST['password'] ?? '');
+
+        $check = array_search($login['email'], array_column($users, 'email'), true);
+
+        // Если впользователь с этим e-mail находится, то для авторизации сверяется пароль
+        if ($check !== false) {
+            $this_user = $users[$check];
+            $verify = password_verify($login['password'], $this_user['password']);
+
+            if ($verify === true) {
+                $_SESSION['user'] = $this_user;
+                header('location: /');
+                exit();
+            } else {
+                $invalid_controls['password'] = 'Вы ввели неверный пароль';
+            }
+        } else {
+        // Если пользователь с этим e-mail не находится, выводится соответствующее сообщение
+            $invalid_controls['email'] = 'Пользователь не найден';
+        }
+    }
 }
 
 echo renderTemplate('templates/top.php', ['html_title' => 'Вход']);
